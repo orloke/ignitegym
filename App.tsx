db6b513d186b1b8691dde4e9a3a6aab323a94f2d
Junior Dering <juniordering@hotmail.com>
2023-11-06 17:09:11 -0400

    feat: use image in signIn

--- App.tsx
+++ App.tsx
@@ -3,13 +3,14 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import { NativeBaseProvider } from 'native-base';
 import { Loading } from '@components/Loading';
 import { THEME } from './src/theme';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontLoaded] =useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar backgroundColor='transparent' translucent barStyle='light-content' />
-      {fontLoaded ? <View/>: <Loading/>}
+      {fontLoaded ? <SignIn/>: <Loading/>}
     </NativeBaseProvider>
   )
 }
