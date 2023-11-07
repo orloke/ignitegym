5f144e44eb0f87d6a366c699a7b31870701a62bf
Junior Dering <juniordering@hotmail.com>
2023-11-06 20:18:12 -0400

    feat: svg component

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,30 @@
-import { Image, VStack } from 'native-base'
+import { Center, Heading, Image, Text, VStack } from 'native-base'
 import BackgroundImg from '@assets/background.png'
+import LogoSvg from '@assets/logo.svg'
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg='gray.700' >
+    <VStack flex={1} bg="gray.700">
       <Image
         source={BackgroundImg}
         alt="Pessoas treinando"
-        resizeMode='contain'
-        position='absolute'
+        resizeMode="contain"
+        position="absolute"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e seu corpo
+        </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
+      </Center>
     </VStack>
   )
 }
