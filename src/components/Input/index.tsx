01a7ef5538ba58324b58282f61167979017b7902
Junior Dering <juniordering@hotmail.com>
2023-11-06 20:26:49 -0400

    feat: inputs

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,25 @@
+import { IInputProps, Input as NativaBaseInput } from 'native-base'
+
+type Props = IInputProps & {}
+
+export function Input({ ...rest }: Props) {
+  return (
+    <NativaBaseInput
+      bg="gray.700"
+      h={14}
+      px={4}
+      borderWidth={0}
+      fontSize="md"
+      color="white"
+      fontFamily="body"
+      mb={4}
+      placeholderTextColor="gray.300"
+      _focus={{
+        bg:"gray.700",
+        borderWidth: 1,
+        borderColor: 'green.500'
+      }}
+      {...rest}
+    />
+  )
+}
