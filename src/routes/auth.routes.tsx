b6cfa6af1ddfeb34c1f1a28d537f79c2c519f25b
Junior Dering <juniordering@hotmail.com>
2023-11-07 15:51:32 -0400

    feat: auth routes

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,21 @@
+import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
+import { View } from 'react-native';
+
+type AuthRoutes = {
+  signIn: undefined,
+  signUp: undefined
+}
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>
+
+export function AuthRoutes() {
+  const {Navigator, Screen} = createNativeStackNavigator<AuthRoutes>()
+  return (
+    <Navigator>
+      <Screen name='signIn' component={SignIn} />
+      <Screen name='signUp' component={SignUp} />
+    </Navigator>
+  );
+};
