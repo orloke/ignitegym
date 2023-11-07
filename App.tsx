94cf9c2003366651976ee294076df39f248a84a3
Junior Dering <juniordering@hotmail.com>
2023-11-06 20:49:17 -0400

    feat: signUp

--- App.tsx
+++ App.tsx
@@ -4,13 +4,14 @@ import { NativeBaseProvider } from 'native-base';
 import { Loading } from '@components/Loading';
 import { THEME } from './src/theme';
 import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [fontLoaded] =useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar backgroundColor='transparent' translucent barStyle='light-content' />
-      {fontLoaded ? <SignIn/>: <Loading/>}
+      {fontLoaded ? <SignUp/>: <Loading/>}
     </NativeBaseProvider>
   )
 }
