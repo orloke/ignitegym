bd375f2f2a91affbe4651b757f1bb0a1ce5a3cee
Junior Dering <juniordering@hotmail.com>
2023-11-09 16:45:42 -0400

    feat: finished home header

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,4 +1,7 @@
-import { HStack, Heading, Text, VStack } from 'native-base'
+import { UserPhoto } from '@components/UserPhoto'
+import { HStack, Heading, Text, VStack, Icon } from 'native-base'
+import { MaterialIcons } from '@expo/vector-icons'
+import { TouchableOpacity } from 'react-native'
 
 type Props = {
   /* props go here */
@@ -6,11 +9,24 @@ type Props = {
 
 export function HomeHeader() {
   return (
-    <HStack bg='gray.600' pt={16} pb={5} px={8} alignItems='center' >
-      <VStack>
-        <Text color='gray.100' fontSize='md'>Olá,</Text>
-        <Heading color='gray.100' fontSize='md'>Júnior Dering</Heading>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: 'https://github.com/orloke.png' }}
+        size={16}
+        alt="imagem do usuário"
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Júnior Dering
+        </Heading>
       </VStack>
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
     </HStack>
   )
 }
