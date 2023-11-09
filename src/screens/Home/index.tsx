433c2826ec74c1e2e5c3f26480849e718a16d289
Junior Dering <juniordering@hotmail.com>
2023-11-09 17:02:24 -0400

    feat: group selected

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,26 @@
-import { HomeHeader } from "@components/HomeHeader";
-import { VStack, Text } from "native-base";
+import { Group } from '@components/Group'
+import { HomeHeader } from '@components/HomeHeader'
+import { VStack, Text, HStack } from 'native-base'
+import { useState } from 'react'
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costa')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <HStack>
+        <Group
+          name="costa"
+          isActive={groupSelected == 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+        <Group
+          name="ombro"
+          isActive={groupSelected == 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
+      </HStack>
     </VStack>
-  );
-};
+  )
+}
