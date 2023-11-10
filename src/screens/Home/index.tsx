afb2dc9d026d23795dd67dce574d82a61738f138
Junior Dering <juniordering@hotmail.com>
2023-11-10 15:47:03 -0400

    feat: card of exercise

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
+import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { VStack, Text, HStack, FlatList } from 'native-base'
+import { VStack, Text, HStack, FlatList, Heading } from 'native-base'
 import { useState } from 'react'
 
 export function Home() {
@@ -26,6 +27,18 @@ export function Home() {
         my={10}
         maxH={10}
       />
+      <VStack flex={1} px={8}>
+        <HStack justifyContent='space-between' mb={5}>
+          <Heading color='gray.200' fontSize='md'>
+            Exercícios
+          </Heading>
+          <Text color='gray.200'>
+            4
+          </Text>
+        </HStack>
+        <ExerciseCard />
+        <ExerciseCard />
+      </VStack>
     </VStack>
   )
 }
