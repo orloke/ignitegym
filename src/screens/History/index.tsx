376e8bfa8e4d6d984c6f573662613b289954b56d
Junior Dering <juniordering@hotmail.com>
2023-11-10 19:03:15 -0400

    feat: history card

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,10 +1,12 @@
+import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack } from "native-base";
+import { HStack, VStack } from "native-base";
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title = 'Histórico de Exercícios' />
+      <HistoryCard />
     </VStack>
   );
 };
