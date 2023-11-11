32e5a0ca7fadf043fcd013bc5586ba291c06b3a9
Junior Dering <juniordering@hotmail.com>
2023-11-11 09:20:50 -0400

    feat: skeleton

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,10 +1,36 @@
 import { ScreenHeader } from '@components/ScreenHeader'
-import { Center, Text, VStack } from 'native-base'
+import { UserPhoto } from '@components/UserPhoto'
+import { Center, Skeleton, Text, VStack } from 'native-base'
+import { useState } from 'react'
+import { ScrollView } from 'react-native'
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(true)
+
   return (
     <VStack flex={1}>
-      <ScreenHeader title='Perfil' />
+      <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.400"
+              endColor="gray.500"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: 'https://github.com/orloke.png' }}
+              alt="imagem do usuario"
+              size={PHOTO_SIZE}
+            />
+          )}
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
