a2bb3d8d6f060fbc4a10df8301744845ffd868aa
Junior Dering <juniordering@hotmail.com>
2023-11-13 18:48:07 -0400

    refactor: refactor code

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,15 +1,14 @@
+import HistorySvg from '@assets/history.svg'
+import HomeSvg from '@assets/home.svg'
+import ProfileSvg from '@assets/profile.svg'
 import {
-  createBottomTabNavigator,
   BottomTabNavigationProp,
+  createBottomTabNavigator,
 } from '@react-navigation/bottom-tabs'
 import { Exercise } from '@screens/Exercise'
 import { History } from '@screens/History'
 import { Home } from '@screens/Home'
 import { Profile } from '@screens/Profile'
-import HomeSvg from '@assets/home.svg'
-import HistorySvg from '@assets/history.svg'
-import ExerciseSvg from '@assets/exercise.svg'
-import ProfileSvg from '@assets/profile.svg'
 import { useTheme } from 'native-base'
 import { Platform } from 'react-native'
 
